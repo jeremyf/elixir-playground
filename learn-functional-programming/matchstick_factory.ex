5d82c732b2e09dc601c0862bb27bf0122ab1ccc4
Jeremy Friesen <github@jeremyfriesen.com>
2025-04-18 13:45:40 -0400

    refactor(): adjust logic

--- learn-functional-programming/matchstick_factory.ex
+++ learn-functional-programming/matchstick_factory.ex
@@ -1,24 +1,23 @@
 defmodule MatchstickFactory do
   # An array of tuples that represent the box type and capacity.
-  @box_types [
-     {:big, 50},
-     {:medium, 20},
-     {:small, 5}
-   ]
+  @box_types [ {:big, 50}, {:medium, 20}, {:small, 5} ]
 
   # Given a number of matchsticks, we process the box types in order.  Filling a
   # number of boxes of the type based on their capacity.  Then, with any
   # remaing.
   def boxes(matchsticks, box_types \\ @box_types) do
-    {boxes_used_by_type, remainder} = Enum.reduce(
-                                        box_types,
-                                        {%{},matchsticks},
-                                        fn({box_type, capacity}, {boxes_used_by_type,unboxed_matchsticks}) ->
-                                          boxes = div(unboxed_matchsticks, capacity)
-                                          boxes_used_by_type = Map.put(boxes_used_by_type, box_type, boxes)
-                                          {boxes_used_by_type, unboxed_matchsticks - boxes * capacity}
-                                        end)
-    boxes_used_by_type = Map.put(boxes_used_by_type, :remaining_matchsticks, remainder)
+    # A little bit of cheating going on.  While technically not a box, our
+    # requirements are such that we need to know the number of match sticks
+    # remaining after boxing up the given matchsticks.
+    box_types = box_types ++ [{:remaining_matchsticks, 1}]
+    {boxes_used_by_type, _} = Enum.reduce(
+                                box_types,
+                                {%{},matchsticks},
+                                fn({box_type, capacity}, {boxes_used_by_type,unboxed_matchsticks}) ->
+                                  boxes = div(unboxed_matchsticks, capacity)
+                                  boxes_used_by_type = Map.put(boxes_used_by_type, box_type, boxes)
+                                  {boxes_used_by_type, unboxed_matchsticks - boxes * capacity}
+                                end)
     boxes_used_by_type
   end
 end
